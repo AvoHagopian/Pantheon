d6a5b3b9c79dd1bf9d1dc673bd6540fb3330042e
AvoHagopian <avedis.hagopian.111@my.csun.edu>
2018-12-02 15:28:28 -0800

    Mock data and CSS

---
+++ src/app/mock-countries.ts
@@ -0,0 +1,14 @@
+import { Country } from './country';
+
+export const COUNTRIES: Country[] = [
+  { id: 0, name: 'A', area: 1, population: 2 },
+  { id: 1, name: 'B', area: 3, population: 1 },
+  { id: 2, name: 'C', area: 1, population: 2 },
+  { id: 3, name: 'D', area: 3, population: 1 },
+  { id: 4, name: 'E', area: 1, population: 2 },
+  { id: 5, name: 'F', area: 3, population: 1 },
+  { id: 6, name: 'G', area: 1, population: 2 },
+  { id: 7, name: 'H', area: 3, population: 1 },
+  { id: 8, name: 'I', area: 1, population: 2 },
+  { id: 9, name: 'J', area: 3, population: 1 },
+];
---
+++ src/app/mock-gods.ts
@@ -0,0 +1,14 @@
+import { God } from './god';
+
+export const GODS: God[] = [
+  { id: 0, country: 1, name: 'A', power: '1', symbol: '2', animal: 'A' },
+  { id: 1, country: 2, name: 'B', power: '3', symbol: '1', animal: 'B' },
+  { id: 2, country: 1, name: 'C', power: '1', symbol: '2', animal: 'C' },
+  { id: 3, country: 2, name: 'D', power: '3', symbol: '1', animal: 'D' },
+  { id: 4, country: 1, name: 'E', power: '1', symbol: '2', animal: 'E' },
+  { id: 5, country: 2, name: 'F', power: '3', symbol: '1', animal: 'F' },
+  { id: 6, country: 1, name: 'G', power: '1', symbol: '2', animal: 'G' },
+  { id: 7, country: 2, name: 'H', power: '3', symbol: '1', animal: 'H' },
+  { id: 8, country: 1, name: 'I', power: '1', symbol: '2', animal: 'I' },
+  { id: 9, country: 2, name: 'J', power: '3', symbol: '1', animal: 'J' },
+];
