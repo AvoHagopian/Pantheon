23bea80cf0ab9a46303318b17d83dfdca23cc319
AvoHagopian <avedis.hagopian.111@my.csun.edu>
2018-12-09 23:58:26 -0800

    WIP

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Country } from '../country';
-import { COUNTRIES } from '../mock-countries';
 import { CountryService } from '../country.service';
 
 @Component({
